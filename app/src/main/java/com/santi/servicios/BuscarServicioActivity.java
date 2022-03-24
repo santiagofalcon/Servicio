c14ebc368a5dbcde8a8442df24fede57ef6a882b
santiagofalcon <santiagofalcon12@hotmail.com>
2022-03-24 17:47:59 +0100

    Servicios Vistas

---
+++ app/src/main/java/com/santi/servicios/BuscarServicioActivity.java
@@ -0,0 +1,14 @@
+package com.santi.servicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuscarServicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_servicio);
+    }
+}
---
+++ app/src/main/java/com/santi/servicios/FormaPagoActivity.java
@@ -0,0 +1,14 @@
+package com.santi.servicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormaPagoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forma_pago);
+    }
+}
